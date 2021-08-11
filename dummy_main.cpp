076d17a3da2dcd96f653c8da20ec11761ef0d932
Martin Franceschi <mfranc.martin@gmail.com>
2021-08-11 17:06:51 +0200

    clean up

--- dummy_main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <memory>
-
-int main() {
-    std::unique_ptr<int> pointer = std::make_unique<int>(42);
-    std::cout << "Hello, world! " << *pointer << std::endl;
-    return EXIT_SUCCESS;
-}
