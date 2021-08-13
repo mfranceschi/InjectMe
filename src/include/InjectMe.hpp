e7efd780b2a71cec8271d0bc1bfa20edc4f791b4
Martin Franceschi <mfranc.martin@gmail.com>
2021-08-13 02:16:21 +0200

    using clang-format

--- src/include/InjectMe.hpp
+++ src/include/InjectMe.hpp
@@ -1,13 +1,13 @@
 
 namespace mf
 {
-    namespace InjectMe
-    {
-        /**
-         * Dummy function for testing purpose.
-         * @return the sum of a and b.
-         * @throws std::overflow_error or std::underflow_error.
-         */
-        int addIntegersOrThrow(int a, int b);
-    } // namespace InjectMe
-} // namespace mf
+  namespace InjectMe
+  {
+    /**
+     * Dummy function for testing purpose.
+     * @return the sum of a and b.
+     * @throws std::overflow_error or std::underflow_error.
+     */
+    int addIntegersOrThrow(int a, int b);
+  }  // namespace InjectMe
+}  // namespace mf
--- src/src/dummy_function.cpp
+++ src/src/dummy_function.cpp
@@ -1,23 +1,21 @@
+#include <limits>
+#include <stdexcept>
+
 #include "InjectMe.hpp"
-#include "limits"
-#include "stdexcept"
 
 namespace mf
 {
-    namespace InjectMe
-    {
-        int addIntegersOrThrow(int a, int b)
-        {
-            // adapted from https://stackoverflow.com/questions/55468823/how-to-detect-integer-overflow-in-c
-            if ((b > 0) && (a > std::numeric_limits<int>::max() - b))
-            {
-                throw std::overflow_error("a+b will overflow");
-            }
-            else if ((b < 0) && (a < std::numeric_limits<int>::min() - b))
-            {
-                throw std::underflow_error("a+b will underflow");
-            }
-            return a + b;
-        }
-    } // namespace InjectMe
-} // namespace mf
+  namespace InjectMe
+  {
+    int addIntegersOrThrow(int a, int b) {
+      // adapted from
+      // https://stackoverflow.com/questions/55468823/how-to-detect-integer-overflow-in-c
+      if ((b > 0) && (a > std::numeric_limits<int>::max() - b)) {
+        throw std::overflow_error("a+b will overflow");
+      } else if ((b < 0) && (a < std::numeric_limits<int>::min() - b)) {
+        throw std::underflow_error("a+b will underflow");
+      }
+      return a + b;
+    }
+  }  // namespace InjectMe
+}  // namespace mf
