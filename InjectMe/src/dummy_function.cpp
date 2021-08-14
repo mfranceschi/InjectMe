bef46bb079360d37be1093693e94edc91dbb8c7f
Martin Franceschi <mfranc.martin@gmail.com>
2021-08-14 20:47:48 +0200

    readability

--- InjectMe/src/dummy_function.cpp
+++ InjectMe/src/dummy_function.cpp
@@ -12,9 +12,12 @@ namespace mf
       // https://stackoverflow.com/questions/55468823/how-to-detect-integer-overflow-in-c
       if ((b > 0) && (a > std::numeric_limits<int>::max() - b)) {
         throw std::overflow_error("a+b will overflow");
-      } else if ((b < 0) && (a < std::numeric_limits<int>::min() - b)) {
+      }
+
+      if ((b < 0) && (a < std::numeric_limits<int>::min() - b)) {
         throw std::underflow_error("a+b will underflow");
       }
+
       return a + b;
     }
   }  // namespace InjectMe
--- InjectMe/src/inject.cpp
+++ InjectMe/src/inject.cpp
@@ -12,7 +12,12 @@ namespace mf
   namespace InjectMe
   {
     Injected<void> injectForTypeOrThrow(const std::type_info& typeInfo) {
-      return Database::getInstance().getForType(std::type_index(typeInfo));
+      try {
+        return Database::getInstance().getForType(std::type_index(typeInfo));
+      } catch (const std::out_of_range&) {
+        throw std::logic_error(
+            "InjectMe::inject - we don't have a provider for the given type (TODO TYPE).");
+      }
     }
   }  // namespace InjectMe
 }  // namespace mf
