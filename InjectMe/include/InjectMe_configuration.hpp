917c0526a1aa371c4baf9821abb2a986018d7cb8
Martin Franceschi <mfranc.martin@gmail.com>
2023-07-15 01:02:25 +0200

    document and clean up

--- InjectMe/include/InjectMe_configuration.hpp
+++ InjectMe/include/InjectMe_configuration.hpp
@@ -23,14 +23,20 @@ namespace mf
         };
       }
 
+      /**
+       * @returns A provider that throws, since the type is not default-constructible.
+       */
       template <typename T, std::enable_if_t<!std::is_default_constructible<T>::value, bool> = true>
       ProviderFct<T> makeDefaultProvider() {
         return []() -> T* {
           throw exceptions::MissingProvider(
-              "configure (for abstract or non-constructible type)", typeid(T).name());
+              "configure (for abstract or non-default-constructible type)", typeid(T).name());
         };
       }
 
+      /**
+       * @returns A provider that uses the default constructor.
+       */
       template <typename T, std::enable_if_t<std::is_default_constructible<T>::value, bool> = true>
       ProviderFct<T> makeDefaultProvider() {
         return []() -> T* {
@@ -64,35 +70,40 @@ namespace mf
         configureWithValueNonTyped(castedValue, castedDeleter, typeid(T));
       }
 
-      template <typename T>
+      template <typename T, class ConfigClass>
       class ConfiguratorBase {
        public:
         ~ConfiguratorBase() = default;
         explicit ConfiguratorBase(const ConfiguratorBase&) = delete;
-        explicit ConfiguratorBase(ConfiguratorBase&&) noexcept = default;
         ConfiguratorBase& operator=(const ConfiguratorBase&) = delete;
+        explicit ConfiguratorBase(ConfiguratorBase&&) noexcept = default;
         ConfiguratorBase& operator=(ConfiguratorBase&&) noexcept = default;
+
+        ConfigClass& setDeleter(const DeleterFct<T>& newDeleter) {
+          static auto DUMMY_DELETER = [](...) {
+          };
+          deleter = (newDeleter ? newDeleter : DUMMY_DELETER);
+          return *((ConfigClass*)this);
+        }
 
         virtual void done() = 0;
 
        protected:
-        ConfiguratorBase<T>() = default;
-        ConfiguratorBase<T>(T* theValue) : value(theValue) {
+        ConfiguratorBase() = default;
+
+        ConfiguratorBase(T* theValue) : value(theValue) {
           if (value == nullptr) {
             throw exceptions::InvalidPointer(
                 "configure", std::string("invalid provided value for type \"") +
                                  std::string(typeid(T).name()) + std::string("\"."));
           }
         }
-        void setDeleterInternal(const DeleterFct<T>& newDeleter) {
-          static auto DUMMY_DELETER = [](...) {
-          };
-          deleter = newDeleter ? newDeleter : DUMMY_DELETER;
-        }
+
         T* getValue() const {
           return value;
         }
-        const DeleterFct<T>& getDeleterInternal() const {
+
+        const DeleterFct<T>& getDeleter() const {
           return deleter;
         }
 
@@ -102,13 +113,10 @@ namespace mf
       };
 
       template <typename T>
-      class ConfiguratorWithProvider : public ConfiguratorBase<T> {
+      class ConfiguratorWithProvider : public ConfiguratorBase<T, ConfiguratorWithProvider<T>> {
        public:
         ConfiguratorWithProvider() = default;
-        ConfiguratorWithProvider& setDeleter(const DeleterFct<T>& newDeleter) {
-          this->setDeleterInternal(newDeleter);
-          return *this;
-        }
+
         ConfiguratorWithProvider& setProvider(const ProviderFct<T>& newProvider) {
           if (!newProvider) {
             throw exceptions::MissingProvider("configuration", typeid(T).name());
@@ -116,8 +124,9 @@ namespace mf
           provider = newProvider;
           return *this;
         }
+
         void done() override {
-          internals::configureWithProvider(provider, this->getDeleterInternal());
+          internals::configureWithProvider(provider, this->getDeleter());
         }
 
        private:
@@ -125,30 +134,52 @@ namespace mf
       };
 
       template <typename T>
-      class ConfiguratorWithValue : public ConfiguratorBase<T> {
+      class ConfiguratorWithValue : public ConfiguratorBase<T, ConfiguratorWithValue<T>> {
        public:
-        explicit ConfiguratorWithValue<T>(T* theValue) : ConfiguratorBase<T>(theValue) {
+        explicit ConfiguratorWithValue<T>(T* theValue)
+            : ConfiguratorBase<T, ConfiguratorWithValue<T>>(theValue) {
         }
-        ConfiguratorWithValue& setDeleter(const DeleterFct<T>& newDeleter) {
-          this->setDeleterInternal(newDeleter);
-          return *this;
-        }
+
         void done() override {
-          internals::configureWithValue(this->getValue(), this->getDeleterInternal());
+          internals::configureWithValue(this->getValue(), this->getDeleter());
         }
       };
     }  // namespace internals
 
+    /**
+     * Uses a provider.
+     * The default provider uses either a default constructor if possible, or a function that
+     * throws. If needed, you can change the deleter.
+     *
+     * Example use:
+     * <code>
+     * configure<int>().setProvider(validProvider).done();
+     * </code>
+     */
     template <typename T>
     auto configure() {
       return internals::ConfiguratorWithProvider<T>();
     }
 
+    /**
+     * Uses a pointer that you created.
+     * If needed, you can change the deleter.
+     *
+     * Example use:
+     * <code>
+     * configureWithValue<int>(validInt).setDeleter(validDeleter).done();
+     * </code>
+     */
     template <typename T>
     auto configureWithValue(T* value) {
       return internals::ConfiguratorWithValue<T>(value);
     }
 
+    /**
+     * Utility function to create a provider for your type.
+     * The returned value is not stored until you use it in a `configure*` function.
+     * Parameters are copied by value.
+     */
     template <typename T, typename... Args>
     ProviderFct<T> makeProvider(Args... args) {
       return [=]() -> T* {
--- InjectMe/include/InjectMe_exceptions.hpp
+++ InjectMe/include/InjectMe_exceptions.hpp
@@ -25,21 +25,34 @@ namespace mf
         std::string resultingString;
       };
 
+      /**
+       * Thrown if the pointer is invalid; often null.
+       */
       class InvalidPointer : public Exception {
        public:
         InvalidPointer(const std::string& failingComponent, const std::string& errorDetails = "");
       };
 
+      /**
+       * No available (default or declared) provider for the given type.
+       */
       class MissingProvider : public Exception {
        public:
         MissingProvider(const std::string& failingComponent, const char* typeName);
       };
 
+      /**
+       * There is more than one provider for that type.
+       */
       class DuplicateProvider : public Exception {
        public:
         DuplicateProvider(const std::string& failingComponent, const char* typeName);
       };
 
+      /**
+       * A recursion has been detected and fails fast.
+       * Example: when building A, we need B, so we build B but to build B we need A.
+       */
       class ProviderRecursion : public Exception {
        public:
         ProviderRecursion(const std::string& failingComponent, const char* typeName);
